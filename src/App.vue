6319813562e15eb808c5f75266614bf1a669a78a
nguyenvui <nvvui90@gmail.com>
2022-10-15 14:19:27 +0700

    Hien thi danh sach cac contact lay ve tu server

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,26 @@
-<script setup>
-//import HelloWorld from './components/HelloWorld.vue'
-//import TheWelcome from './components/TheWelcome.vue'
-</script>
 <script>
+import AppHeader from "@/components/AppHeader.vue";
+
 export default {
-}
+    components: {
+        AppHeader,
+    },
+};
 </script>
 
 <template>
-<h1>Hello, Vue.js!</h1>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <div id="app">
+        <AppHeader />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+        <div class="container mt-3">
+            <router-view />
+        </div>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
 </template>
-<style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
+<style>
 .page {
-max-width: 400px;
-margin: auto;
+  max-width: 400px;
+  margin: auto;
 }
-
 </style>
--- vite.config.js
+++ vite.config.js
@@ -9,9 +9,16 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    },},
-    server: {
-      port: 3001,
+    },
+  },
+  server: {
+    port: 3001,
+    proxy: {
+    "/api": {
+    target: "http://localhost:3000/",
+    changeOrigin: true,
+  },
+}
   },
 
 });
